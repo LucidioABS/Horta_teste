b95885613c1344f9a71d31010e5f6368a78d35e3
Lucídio Andrade Barbosa deSouza <lucidio.abs@gmail.com>
2018-12-17 09:29:46 -0400

    Initial Commit

---
+++ horta_teste WatchKit Extension/NotificationController.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationController.swift
+//  horta_teste WatchKit Extension
+//
+//  Created by Lucídio Andrade Barbosa de Souza on 17/12/18.
+//  Copyright © 2018 Lucídio Andrade Barbosa de Souza. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
---
+++ horta_teste/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  horta_teste
+//
+//  Created by Lucídio Andrade Barbosa de Souza on 17/12/18.
+//  Copyright © 2018 Lucídio Andrade Barbosa de Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
